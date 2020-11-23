d0aee05a8578733ef8edb3bff998877edb5e8715
sklemenc <djeiraji@gmail.com>
2020-11-23 23:46:37 +0000

    styling

--- src/RomanNumber.js
+++ src/RomanNumber.js
@@ -1,27 +1,4 @@
-const romanNumbersTokens = new Map();
-romanNumbersTokens.set("M", 1000);
-romanNumbersTokens.set("D", 500);
-romanNumbersTokens.set("C", 100);
-romanNumbersTokens.set("L", 50);
-romanNumbersTokens.set("X", 10);
-romanNumbersTokens.set("V", 5);
-romanNumbersTokens.set("I", 1);
-
-const romanIntValues = new Map();
-
-romanIntValues.set(1000, "M");
-romanIntValues.set(900, "CM");
-romanIntValues.set(500, "D");
-romanIntValues.set(400, "CD");
-romanIntValues.set(100, "C");
-romanIntValues.set(90, "XC");
-romanIntValues.set(50, "L");
-romanIntValues.set(40, "XL");
-romanIntValues.set(10, "X");
-romanIntValues.set(9, "IX");
-romanIntValues.set(5, "V");
-romanIntValues.set(4, "IV");
-romanIntValues.set(1, "I");
+import { romanIntValues, romanNumbersTokens } from './constants'
 
 export default class RomanNumber {
     constructor(value) {
