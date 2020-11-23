995dfc1363c0ba3714629bcce5633dbc51d86b64
sklemenc <djeiraji@gmail.com>
2020-11-23 22:30:46 +0000

    10 minutes push

--- test/RomanNumber.test.js
+++ test/RomanNumber.test.js
@@ -1,23 +1,28 @@
 import { it } from '@jest/globals'
 import RomanNumber from '../src/RomanNumber'
 
+const valueRequiredMessage = 'value required'
+const invalidRangeMessage = 'invalid range'
+const invalidValueMessage = 'invalid value'
+
 
 //mandatory test cases
 it("should throw 'value required' for null", () => {
-    expect(() => { new RomanNumber(null) }).throw('value required')
+    expect(() => { new RomanNumber(null) }).toThrow(valueRequiredMessage)
 })
 
 it("should throw 'value required' for ''", () => {
-
+    expect(() => { new RomanNumber('') }).toThrow(valueRequiredMessage)
 })
 
 it("should throw 'invalid range' for 0", () => {
-
+    expect(() => { new RomanNumber(0) }).toThrow(invalidRangeMessage)
 })
 
 it("should return 'I' for 1", () => {
-
+    expect(new RomanNumber('I').intValue()).toBe(1)
 })
+
 
 it("should return 'III' for 3", () => {
 
@@ -40,7 +45,7 @@ it("should return 3 for 'III' ", () => {
 })
 
 it("should throw 'invalid value' for 'IIII' ", () => {
-
+    expect(() => { new RomanNumber('IIII') }).toThrow(invalidValueMessage)
 })
 
 it("should return 4 for 'IV' ", () => {
@@ -67,7 +72,7 @@ it("should return 'MMM' for 3000 ", () => {
 })
 
 it("should throw 'invalid range' for 10000 ", () => {
-
+    expect(() => { new RomanNumber(10000) }).toThrow(invalidRangeMessage)
 })
 
 it("should return 429 for 'CDXXIX'  ", () => {
@@ -75,6 +80,7 @@ it("should return 429 for 'CDXXIX'  ", () => {
 })
 
 it("should throw 'invalid value' for 'CD1X'  ", () => {
+    expect(() => { new RomanNumber('CD1X') }).toThrow(invalidValueMessage)
 
 })
 
@@ -91,11 +97,11 @@ it("should return 1980 for 'MCMLXXX'  ", () => {
 })
 
 it("should throw 'invalid range' for 'MMMMCMXCIX'  ", () => {
-
+    expect(() => { new RomanNumber('MMMMCMXCIX') }).toThrow(invalidRangeMessage)
 })
 
 it("should throw 'invalid range' for 'MMMMDMXCIX'  ", () => {
-
+    expect(() => { new RomanNumber('MMMMCMXCIX') }).toThrow(invalidRangeMessage)
 })
 
 
